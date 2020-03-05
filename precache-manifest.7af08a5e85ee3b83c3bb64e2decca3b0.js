9aeefaf46379fcf17be234bdd65c9c3f154a6f48
TI OS MAIS FODA <TI@CAMARAPL.local>
2020-03-05 11:52:09 -0300

    Updates

---
+++ precache-manifest.7af08a5e85ee3b83c3bb64e2decca3b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59cd56fd71bb0254bb082b3870a84df1",
+    "url": "/portifolio_site/index.html"
+  },
+  {
+    "revision": "947913c3de661c97581c",
+    "url": "/portifolio_site/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "60d282251bb3e5a8b6b9",
+    "url": "/portifolio_site/static/js/2.03c46741.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/portifolio_site/static/js/2.03c46741.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "947913c3de661c97581c",
+    "url": "/portifolio_site/static/js/main.7ec87e0d.chunk.js"
+  },
+  {
+    "revision": "691127213d6903c2034f",
+    "url": "/portifolio_site/static/js/runtime-main.82d1287c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portifolio_site/static/media/logo.5d5d9eef.svg"
+  }
+]);
